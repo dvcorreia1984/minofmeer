8ae918c1df8c760535fd04f3d0327c392cfbfbeb
dvcorreia1984 <victor@onsleer.co.za>
2024-11-20 08:51:05 +0200

    Update front page image

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -42,7 +42,7 @@ const Index = () => {
             </CardHeader>
             <CardContent className="flex flex-col items-center">
               <img 
-                src="/voortreklogo.jpg" 
+                src="/kombuis.jpg" 
                 alt="Houtwerk" 
                 className="w-full h-48 object-cover rounded-md mb-6"
               />
